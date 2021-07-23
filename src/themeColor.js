2a1abf957e4ac2ca067d248909c9349b15804e34
abdul_rachman <abdul.rachman@logique.co.id>
2021-07-23 15:06:21 +0700

    feat: theme color

---
+++ src/themeColor.js
@@ -0,0 +1,69 @@
+export const themeColor ={
+    typography: {
+        fontFamily: ['Raleway'].join(',')
+    },
+    palette: {
+        primary: {
+            light: '#555cdd',
+            main: '#0033aa',
+            dark: '#000f7a',
+            contrastText: '#ffffff',
+            contrastTextBlack: '#000000',
+    
+          },
+          secondary: {
+            light: '#ffa147',
+            main: '#fe7011',
+            dark: '#c34000',
+            contrastText: '#ffffff',
+            contrastTextBlack: '#000000'
+          },
+          tertiary: {
+            light:'#eaa2d1',
+            main: '#b772a0',
+            dark: '#864571'
+          },
+    
+          error: {
+            light: '#bf334c',
+            main: '#B00020',
+            dark: '#7b0016',
+            contrastText: '#000000',
+          },
+          warning: {
+            light: '#ffee33',
+            main: '#ffea00',
+            dark: '#b2a300',
+            contrastText: '#000000',
+          },
+          success: {
+            light: '#91ff35',
+            main: '#76ff03',
+            dark: '#52b202',
+            contrastText: '#ffffff',
+          },
+          common: {
+            white: '#ffffff',
+            black: '#000000',
+            gray: '#bdbdbd'
+            // ghost: '#f3f3f3'
+          } 
+    },
+  
+  }
+
+// import RalewayWoff2 from './fonts/Raleway-Regular.woff2';
+
+// const raleway = {
+//   fontFamily: 'Raleway',
+//   fontStyle: 'normal',
+//   fontDisplay: 'swap',
+//   fontWeight: 400,
+//   src: `
+//     local('Raleway'),
+//     local('Raleway-Regular'),
+//     url(${RalewayWoff2}) format('woff2')
+//   `,
+//   unicodeRange:
+//     'U+0000-00FF, U+0131, U+0152-0153, U+02BB-02BC, U+02C6, U+02DA, U+02DC, U+2000-206F, U+2074, U+20AC, U+2122, U+2191, U+2193, U+2212, U+2215, U+FEFF',
+// };
